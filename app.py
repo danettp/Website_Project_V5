0e4e811fbc098de0b527b1b23b43eac68813234e
danettp <17204@my.sanctamaria.school.nz>
2023-07-06 20:21:19 +1200

    Made function to hex image file name
    Made function in views.py to randomize image

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     app = create_app()  # Create a Flask application instance using the create_app function
     app.run(debug=True) # Run the Flask application in debug mode
 
-# Adding avatar image/account page 2
+# Adding avatar image/account page 2 5:47
